eff9dc2e3417aa361e2d1ea9f6c6bc6e885d59ec
F Varas <fvaras@anevia.com>
2015-05-22 15:53:40 +0200

    now prints humar readable start and end date

--- iss.py
+++ iss.py
@@ -1,9 +1,15 @@
 import requests
 import json
+import datetime
 
+
+# you can get lat&lon from wikipedia from the nearest train 
+# station or so
 lat = 48.724114
 lon = 2.259434
+# altitude is just BS
 alt = 100
+# how many future passes, further passes are less precise and can change
 passes = 2
 baseurl = "http://api.open-notify.org/iss-pass.json"
 query = "lat=" + str(lat) + "&lon=" + str(lon) + "&alt=" + str(alt) + "&n=" + str(passes)
@@ -16,4 +22,13 @@ dldata = req.text
 data = json.loads(dldata)
 
 response = data['response']
-print(response)
+#print(response)
+
+for i in response:
+  #print(i)
+  duration = i['duration']
+  risetime = i['risetime']
+  begintime = lambda x:datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S')
+  endtime = lambda x, y:datetime.datetime.fromtimestamp(x + y).strftime('%Y-%m-%d %H:%M:%S') 
+  print("Station pass starts at: {0} and ends at: {1}".format(begintime(risetime),endtime(risetime, duration)))
+
